50accd6d6d34674132df1164c271a73dfb4a3db3
Ray Bellis <ray@isc.org>
2019-01-23 16:58:43 +0000

    thenify the SessionStore

--- src/node/db/SessionStore.js
+++ src/node/db/SessionStore.js
@@ -8,12 +8,13 @@ var Store = require('ep_etherpad-lite/node_modules/express-session').Store,
   db = require('ep_etherpad-lite/node/db/DB').db,
   log4js = require('ep_etherpad-lite/node_modules/log4js'),
   messageLogger = log4js.getLogger("SessionStore");
+const thenify = require("thenify").withCallback;
 
 var SessionStore = module.exports = function SessionStore() {};
 
 SessionStore.prototype.__proto__ = Store.prototype;
 
-SessionStore.prototype.get = function(sid, fn){
+SessionStore.prototype.get = thenify(function(sid, fn){
   messageLogger.debug('GET ' + sid);
   var self = this;
   db.get("sessionstorage:" + sid, function (err, sess)
@@ -29,25 +30,25 @@ SessionStore.prototype.get = function(sid, fn){
       fn();
     }
   });
-};
+});
 
-SessionStore.prototype.set = function(sid, sess, fn){
+SessionStore.prototype.set = thenify(function(sid, sess, fn){
   messageLogger.debug('SET ' + sid);
   db.set("sessionstorage:" + sid, sess);
   process.nextTick(function(){
     if(fn) fn();
   });
-};
+});
 
-SessionStore.prototype.destroy = function(sid, fn){
+SessionStore.prototype.destroy = thenify(function(sid, fn){
   messageLogger.debug('DESTROY ' + sid);
   db.remove("sessionstorage:" + sid);
   process.nextTick(function(){
     if(fn) fn();
   });
-};
+});
 
-SessionStore.prototype.all = function(fn){
+SessionStore.prototype.all = thenify(function(fn){
   messageLogger.debug('ALL');
   var sessions = [];
   db.forEach(function(key, value){
@@ -56,9 +57,9 @@ SessionStore.prototype.all = function(fn){
     }
   });
   fn(null, sessions);
-};
+});
 
-SessionStore.prototype.clear = function(fn){
+SessionStore.prototype.clear = thenify(function(fn){
   messageLogger.debug('CLEAR');
   db.forEach(function(key, value){
     if (key.substr(0,15) === "sessionstorage:") {
@@ -66,9 +67,9 @@ SessionStore.prototype.clear = function(fn){
     }
   });
   if(fn) fn();
-};
+});
 
-SessionStore.prototype.length = function(fn){
+SessionStore.prototype.length = thenify(function(fn){
   messageLogger.debug('LENGTH');
   var i = 0;
   db.forEach(function(key, value){
@@ -77,4 +78,4 @@ SessionStore.prototype.length = function(fn){
     }
   });
   fn(null, i);
-};
+});
